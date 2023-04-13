298b24db51fc1177a6717e18e5e1b299abbac255
Magsigmus <61921471+Magsigmus@users.noreply.github.com>
2023-04-13 04:53:09 +0200

    Started on tilemap, added some old scripts

---
+++ HowtGuy/Assets/Old scripts/Topdown/GunPointer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPointer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+    public float distToPlayer =1;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - gameObject.transform.parent.position;
+        dir = dir.normalized * distToPlayer;
+        gameObject.transform.localPosition = dir;
+        float angleZ = Mathf.Acos(dir.normalized.x) * Mathf.Sign(dir.y) * Mathf.Rad2Deg;
+
+        float angleY = 180 * Mathf.Floor(Mathf.Abs(angleZ / 90));
+        gameObject.transform.localEulerAngles = new Vector3(0,0,angleZ);
+        gameObject.transform.GetChild(0).localEulerAngles = new Vector3(angleY, 0, 0);
+    }
+}
