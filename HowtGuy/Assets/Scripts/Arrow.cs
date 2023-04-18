f29999c2d6cfeae15492d428168bca3f460ea7c8
HaakonSkov <130569276+HaakonSkov@users.noreply.github.com>
2023-04-18 11:24:05 +0200

    I have been playing with arrow

--- HowtGuy/Assets/Scripts/Arrow.cs
+++ HowtGuy/Assets/Scripts/Arrow.cs
@@ -5,22 +5,12 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float charge;
-    public int Modifyer;
-    public Rigidbody2D rb;
-    public GameObject Shulder;
-    public BowPointer BP;
+    
+    Rigidbody2D rb;
 
     void Start()
     {
-        Shulder = GameObject.Find("ShulderPoint");
-        BP = Shulder.GetComponent<BowPointer>();
-
-        charge = BP.Charge;
-        Debug.Log("Charge level: " + charge);
         rb = GetComponent<Rigidbody2D>();
-
-        //rb.velocity = new Vector2(Mathf.Cos(transform.rotation.z) * charge * Modifyer, Mathf.Sign(transform.rotation.z) * charge * Modifyer);
     }
 
     // Update is called once per frame
@@ -28,4 +18,17 @@ public class Arrow : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+         
+        if (collision.gameObject.tag == "Ground")
+        {
+            rb.velocity = new Vector2(0, 0);
+            rb.gravityScale = 0;
+            transform.SetParent(collision.gameObject.transform, true);
+        }
+    }
+
+
 }
