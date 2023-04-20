86713895cc9c927a291866abf3e6461ea981bf13
HaakonSkov <130569276+HaakonSkov@users.noreply.github.com>
2023-04-20 09:00:45 +0200

    Extra tuch up

--- HowtGuy/Assets/Scripts/BowPointer.cs
+++ HowtGuy/Assets/Scripts/BowPointer.cs
@@ -16,7 +16,7 @@ public class BowPointer : MonoBehaviour
     public float TimeToCharge;
     public float TimeInExtraCharge;
     public float EndCharge;
-    public float ChargeTime;
+    float ChargeTime;
 
     // Start is called before the first frame update
     void Start()
